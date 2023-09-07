a7b04f1cc3c8d8910f95b84534e621ba6853cc80
Yannis Kotsinos <sinnay@geekbot.com>
2023-09-07 12:53:42 +0300

    modify seed so it accepts flags & add a producst seed

--- src/commands/dbseed.go
+++ src/commands/dbseed.go
@@ -3,12 +3,18 @@ package main
 import (
 	"ambassador/src/database"
 	"ambassador/src/models"
+	"flag"
 	"fmt"
+	"math/rand"
 
+	"github.com/brianvoe/gofakeit/v6"
 	"github.com/go-faker/faker/v4"
 )
 
 func main() {
+	seedAmbassadors := flag.Bool("ambassadors", false, "Seed ambassadors")
+	seedProducts := flag.Bool("products", false, "Seed products")
+	flag.Parse()
 
 	db, err := database.NewDatabase()
 	if err != nil {
@@ -16,28 +22,44 @@ func main() {
 		return
 	}
 
-	var count int64
-	if err := db.DB.Model(&models.UserRead{}).Where("is_ambassador = ?", true).Count(&count).Error; err != nil {
-		// Handle the error, e.g., log it or return an error response
-		fmt.Println("Error counting records:", err)
-		return
-	}
-
-	if count > 0 {
-		fmt.Println("Data already exists, skipping seeding")
-		return
-	}
-
-	for i := 0; i < 1; i++ {
-		ambassador := models.UserWrite{
-			User: models.User{
-				FirstName:    faker.FirstName(),
-				LastName:     faker.LastName(),
-				Email:        faker.Email(),
-				IsAmbassador: true,
-			},
+	if *seedAmbassadors {
+		var count int64
+		if err := db.DB.Model(&models.UserRead{}).Where("is_ambassador = ?", true).Count(&count).Error; err != nil {
+			// Handle the error, e.g., log it or return an error response
+			fmt.Println("Error counting records:", err)
+			return
 		}
-		ambassador.SetPassword("1234")
-		db.DB.Create(&ambassador)
+
+		if count > 0 {
+			fmt.Println("Data already exists, skipping seeding")
+			return
+		}
+
+		for i := 0; i < 1; i++ {
+			ambassador := models.UserWrite{
+				User: models.User{
+					FirstName:    faker.FirstName(),
+					LastName:     faker.LastName(),
+					Email:        faker.Email(),
+					IsAmbassador: true,
+				},
+			}
+			ambassador.SetPassword("1234")
+			db.DB.Create(&ambassador)
+		}
+	}
+
+	if *seedProducts {
+		for i := 0; i < 1; i++ {
+			product := models.ProductWrite{
+				Product: models.Product{
+					Title:       gofakeit.BuzzWord(),         // Generates a random buzzword as the product title
+					Description: gofakeit.Sentence(5),        // Generates a random sentence as the product description
+					Image:       gofakeit.ImageURL(100, 100), // Generates a random image URL
+					Price:       rand.Float64(),
+				},
+			}
+			db.DB.Create(&product)
+		}
 	}
 }
